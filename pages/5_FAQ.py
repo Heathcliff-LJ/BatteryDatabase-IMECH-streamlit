81aaa0d7588cc637192588567c5f50e70b4381ef
Heathcliff-LJ <liujin@imech.ac.cn>
2024-09-25 20:45:42 +0800

    Update 5_FAQ.py

--- pages/5_FAQ.py
+++ pages/5_FAQ.py
@@ -12,6 +12,8 @@ st.markdown("""
         -Charge-rate-Upper Voltage-Discharge-rate-Lower Voltage-Working Temperature-Cell Number of repeated experiment".
          __For example:__"LG1-18-NCA-3.35-1_4.2-3_2.5-T25-1" represents the first batch 18650 cell with NCA cathode and 3.35
         Ah nominal capacity from LG, which charged at 1C to 4.2V and discharged to 2.5V at 3C in an environment of 25 degree.
+        ### Q: How how can I get access to the raw data ?
+        A: To offer site feedback or contribute datasets, please email：liujin@imech.ac.cn
         ### Q: How do I contribute data to the site ?
         A: To offer site feedback or contribute datasets, please email：liujin@imech.ac.cn
         
