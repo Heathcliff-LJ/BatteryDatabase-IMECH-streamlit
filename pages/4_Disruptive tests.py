223de45e7ed7940d49199f5b0c51c2bed449c809
Heathcliff-LJ <liujin@imech.ac.cn>
2024-09-25 20:14:17 +0800

    Update 4_Disruptive tests.py

--- pages/4_Disruptive tests.py
+++ pages/4_Disruptive tests.py
@@ -5,10 +5,10 @@ st.header('Preparing...')
 col1, col2, col3 = st.columns(3)
 with col1:
     # st.write("Cycling")
-    st.image('pics\Disruptive.png')
+    st.image('pics/Disruptive.png')
 with col2:
     # st.write("Characterization")
-    st.image('pics\Disruptive2.jpg')
+    st.image('pics/Disruptive2.jpg')
 with col3:
     # st.write("Disruptive")
-    st.image('pics\Disruptive3.jpg')
+    st.image('pics/Disruptive3.jpg')
